aa4a43fc59b9ca0dffb8f96fc841987ddd6e4c6f
Basavaraj6177 <109595120+Basavaraj6177@users.noreply.github.com>
2024-10-27 15:08:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    window.onscroll = function() {
+        var navbar = document.getElementById("navbar");
+        var headerHeight = document.querySelector(".conference-header").offsetHeight;
+
+        // Check if the page is scrolled beyond the header height
+        if (window.scrollY > headerHeight) {
+            navbar.classList.add("fixed-top");
+        } else {
+            navbar.classList.remove("fixed-top");
+        }
+    };
+});
