e9b8b81dad0be95b327f8b2be13ee1697967c4c6
Basavaraj6177 <109595120+Basavaraj6177@users.noreply.github.com>
2024-10-28 13:00:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+function changeReadMore() {
+    const mycontent =
+        document.getElementById('mybox1id');
+    const mybutton =
+        document.getElementById('mybuttonid');
+
+    if (mycontent.style.display === 'none'
+        || mycontent.style.display === '') {
+        mycontent.style.display = 'inline';
+        mybutton.textContent = 'read less';
+    } else {
+        mycontent.style.display = 'none';
+        mybutton.textContent = 'read more...';
+    }
+}
